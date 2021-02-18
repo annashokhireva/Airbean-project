70e9cb5a0c22db99b95e1038c39cb4d1308c4746
nicole pena fuentes <nicole.pena.fuentes@iths.se>
2021-02-18 13:19:37 +0100

    meny, footer

--- src/views/MenuPage.vue
+++ src/views/MenuPage.vue
@@ -1,5 +1,11 @@
 <template>
+<section class="header">
+</section>
+<div>	
 	<h1>Menu</h1>
+</div>
+<section class="footer">
+</section>
 </template>
 
 <script>
@@ -8,6 +14,23 @@ export default {
 }
 </script>
 
-<style>
+
+
+<style lang="scss" scoped>
+main {
+	width: 100%;
+	height: 100%;
+	background-size: cover;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	cursor: pointer;
+}
+
+.header{
+	background-image: url("../assets/header.svg");
+	margin-top: 0%;
+}
 
 </style>
