1921f2d3b5f91a1a7c5fdab482bcdbddf8ae66c7
nicole pena fuentes <nicole.pena.fuentes@iths.se>
2021-02-18 20:07:06 +0100

    gjort meny och försökt göra increment counter

---
+++ src/components/MainFooter.vue
@@ -0,0 +1,26 @@
+<template>
+    <footer>
+		<img src="../assets/footer.svg" class="fix">
+    </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+	footer {
+		background-image: url("../assets/footer.svg");
+		background-size: 100%;
+		background-repeat: no-repeat;
+		width: 100%;
+		min-height: 113px;
+        padding-bottom: 0%;     
+       
+	}
+
+
+</style>
+
--- src/views/MenuPage.vue
+++ src/views/MenuPage.vue
@@ -1,36 +1,67 @@
 <template>
-<section class="header">
-</section>
-<div>	
-	<h1>Menu</h1>
+
+<div id="MenuPage">
+
+	<div class="product-menu">
+		<main-header />
+			<h1>Meny </h1>
+			<h1>Count - {{count}}</h1>
+		<button @click="increment">Increment</button>
+	
+		<main-footer />	
+	</div>
 </div>
-<section class="footer">
-</section>
+	
 </template>
 
 <script>
-export default {
 
+import MainHeader from '../components/MainHeader';
+import MainFooter from '../components/MainFooter';
+
+export default {
+	components: { 
+		MainHeader,
+		MainFooter,
+	},
 }
 </script>
 
 
-
 <style lang="scss" scoped>
-main {
-	width: 100%;
-	height: 100%;
-	background-size: cover;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-	cursor: pointer;
-}
+	.product-menu {
+		height: 100%;
+		width: 100%;
+		overflow: scroll;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		background-color: $airbean_pink;
+	}
 
-.header{
-	background-image: url("../assets/header.svg");
-	margin-top: 0%;
-}
+	main {
+		height: fit-content;
+		padding: 0 10%;
+		text-align: left;
+	}
+
+	h1 {
+		font-size: $h_big;
+		width: 100%;
+		margin: 5% 0;
+	}
+
+	h2 {
+	font-size: $h_big;
+		width: 100%;
+		margin: 5% 0;
+
+	}
+	
+	p {
+		font-size: $para;
+	}
+
 
 </style>
+
