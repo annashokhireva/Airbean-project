9cce6ece744d3ad37ff395685e8eabcefc649ae1
nicole pena fuentes <nicole.pena.fuentes@iths.se>
2021-02-18 23:10:37 +0100

    css och knapp till Logga in/ registrera

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,11 +1,20 @@
 <template>
   
-  <div id="profil">
-  <main-header />
-   
-        <h2>Välkommen till airben</h2>
-    
-    </div>
+  <section class="body">
+    <main-header />
+		<container class="registration-form">
+			<h2>Välkommen till AirBean-familjen!</h2>	
+			<p>Genom att skapa ett konto nedan kan du spara och se din orderhistorik.</p>
+			<button class="register">Brew me a cup!</button>	
+			<ValidationCheckbox
+				type="checkbox"
+				label="GDPR"
+				name="terms"
+				validation="accepted"
+			/>
+		</container>
+  </section>
+	
 </template>
 
 <script>
@@ -17,27 +26,54 @@ export default {
 		MainHeader,
 	},
 }
+
+
 </script>
 
 
 <style lang="scss" scoped>
-	.profil {
+	.body{
+		background-color: black;
 		height: 100%;
 		width: 100%;
-		overflow: scroll;
+	}
+
+	.registration-form{
+		height: 80%;
+		width: 90%;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		background-color: $airbean_pink;
+		margin: auto;
+		border-radius: 3px;
+		
 	}
 
-	main {
-		height: fit-content;
-		padding: 0 10%;
-		text-align: left;
+	.registration-form h2 {
+		font-family: PT serif;
+		font-size: 32px;
+		color: $airbean_brown;
+		
 	}
+	.registration-form p {
+		font-family: Work Sans;
+		font-size: 16px;
+		color: $airbean_brown;
+	}
+
+	.register {
+		background-color: $airbean_brown;
+		border-radius: 20px;
+		font-size: 24px ;
+		color: white;
+		padding: 5px 10px 5px 10px;
+		font-family: PT serif;
+	}
+
 
 	h1 {
+		font-family: PT serif;
 		font-size: $h_big;
 		width: 100%;
 		margin: 5% 0;
